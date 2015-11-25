8edad597ca41ef85a59d7ea88f90f60a4f593bb3
sburns <sburns@ancestry.com>
2015-11-25 12:23:19 -0700

    first commit. Contains main app and JavaJokes Java library.

---
+++ javajokes/src/main/java/me/steveburns/JavaJokes.java
@@ -0,0 +1,11 @@
+package me.steveburns;
+
+public class JavaJokes {
+
+    public static String returnAJoke() {
+        return "Why did the chicken cross the road?";
+    }
+    public static String returnAnAnswer() {
+        return "To get to the other side. :)";
+    }
+}
